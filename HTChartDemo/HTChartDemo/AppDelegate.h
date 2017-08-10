979c6c0adf88fa5e68da9c9320fe017f4e627b51
chenghong <chenghong@chenghongtekimac-mini.local>
2017-08-10 14:40:32 +0800

    first commit

---
+++ HTChartDemo/HTChartDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTChartDemo
+//
+//  Created by chenghong on 2017/8/10.
+//  Copyright © 2017年 HT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
